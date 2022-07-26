0c29d56dcb67b3e2f550e619508d680a59720445
Thomas Chevron <chevron.thomas33@gmail.com>
2022-07-26 16:58:13 +0200

    First push final project school

---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingPageController extends AbstractController
+{
+    /**
+     * @Route("/", name="landing")
+     */
+
+    public function landingPage() {
+    
+    }
+
+}
